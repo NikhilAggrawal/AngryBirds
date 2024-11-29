4cb5f335f521d69c40ac42b53c8539fa6fc476bb
NikhilAggrawal <nikhil23347@iiitd.ac.in>
2024-11-29 14:29:20 +0530

    Music button added

--- core/src/main/java/com/birds/game/PauseScreen.java
+++ core/src/main/java/com/birds/game/PauseScreen.java
@@ -54,18 +54,23 @@ public class PauseScreen implements Screen {
         messageImage.setSize(900, 200);
         stage.addActor(messageImage);
 
-
-        Button soundbutton = new Button(new TextureRegionDrawable(new TextureRegion(new Texture("soundon.png"))));
+        Button soundbutton = null;
+        if (AngryBirds1.soundOn) {
+            soundbutton = new Button(new TextureRegionDrawable(new TextureRegion(new Texture("soundon.png"))));
+        }else{
+            soundbutton = new Button(new TextureRegionDrawable(new TextureRegion(new Texture("soundoff.png"))));
+        }
         soundbutton.setPosition(stage.getWidth() - 100, stage.getHeight() - 100);
         soundbutton.setSize(100, 100);
         soundbutton.setTouchable(Touchable.enabled);
         soundbutton.setOrigin(soundbutton.getWidth() / 2, soundbutton.getHeight() / 2);
         soundbutton.setTransform(true);
 
+        Button finalSoundbutton = soundbutton;
         soundbutton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                soundbutton.addAction(Actions.sequence(
+                finalSoundbutton.addAction(Actions.sequence(
                     Actions.scaleTo(0.9f, 0.9f, 0.05f),
                     Actions.scaleTo(1f, 1f, 0.05f)
                 ));
@@ -73,11 +78,11 @@ public class PauseScreen implements Screen {
                 if (AngryBirds1.soundOn) {
                     AngryBirds1.soundOn = false;
                     game.backgroundMusic.stop();
-                    soundbutton.getStyle().up = new TextureRegionDrawable(new Texture(Gdx.files.internal("soundoff.png")));
+                    finalSoundbutton.getStyle().up = new TextureRegionDrawable(new Texture(Gdx.files.internal("soundoff.png")));
                 } else {
                     AngryBirds1.soundOn = true;
                     game.backgroundMusic.play();
-                    soundbutton.getStyle().up = new TextureRegionDrawable(new TextureRegion(new Texture("soundon.png")));
+                    finalSoundbutton.getStyle().up = new TextureRegionDrawable(new TextureRegion(new Texture("soundon.png")));
 
 
                 }
@@ -85,12 +90,12 @@ public class PauseScreen implements Screen {
 
             @Override
             public void enter(InputEvent event, float x, float y, int pointer, com.badlogic.gdx.scenes.scene2d.Actor fromActor) {
-                soundbutton.addAction(Actions.scaleTo(1.1f, 1.1f, 0.1f));
+                finalSoundbutton.addAction(Actions.scaleTo(1.1f, 1.1f, 0.1f));
             }
 
             @Override
             public void exit(InputEvent event, float x, float y, int pointer, com.badlogic.gdx.scenes.scene2d.Actor toActor) {
-                soundbutton.addAction(Actions.scaleTo(1f, 1f, 0.1f));
+                finalSoundbutton.addAction(Actions.scaleTo(1f, 1f, 0.1f));
             }
         });
 
